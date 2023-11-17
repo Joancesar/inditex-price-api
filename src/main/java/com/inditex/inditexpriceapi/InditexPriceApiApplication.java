4ad250c8cf637660a66c8354ccb9f93e40ae6760
Joan Cesar <joancesar@hotmail.com>
2023-11-17 01:27:44 +0100

    first commit

---
+++ src/main/java/com/inditex/inditexpriceapi/InditexPriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.inditexpriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexPriceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InditexPriceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inditex/inditexpriceapi/InditexPriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.inditexpriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexPriceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
